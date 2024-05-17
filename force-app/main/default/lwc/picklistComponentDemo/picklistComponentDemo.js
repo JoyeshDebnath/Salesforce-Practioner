190ae722de392db33813c58bf0c564293322a745
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2024-05-17 22:46:19 +0530

    resusable Picklist demo

--- force-app/main/default/lwc/picklistComponentDemo/picklistComponentDemo.js
+++ force-app/main/default/lwc/picklistComponentDemo/picklistComponentDemo.js
@@ -1,31 +1,54 @@
 //Default record Type id that salesforce provides us :-->>                      012000000000000AAA
 
 import { LightningElement,wire,api } from 'lwc';
-import { getPicklistValues } from 'lightning/uiObjectInfoApi';
-import ACCOUNT_OBJECT from '@salesforce/schema/Account';
-import ACCOUNT_SOURCE_FIELD from '@salesforce/schema/Account.AccountSource';
+import { getPicklistValues,getPicklistValuesByRecordType } from 'lightning/uiObjectInfoApi';
+// import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+// import ACCOUNT_SOURCE_FIELD from '@salesforce/schema/Account.AccountSource';
 const DEFAULT_RECORD_TYPE_ID = '012000000000000AAA';
 
 
 export default class PicklistComponentDemo extends LightningElement {
-    recordTypeId = DEFAULT_RECORD_TYPE_ID;
+    @api recordTypeId = DEFAULT_RECORD_TYPE_ID;
+    @api objectApiName = 'Account';
+    @api fieldApiName = 'Type';
+
     acntSrc;
     options=[];
     
-    @wire(getPicklistValues, {
+    // @wire(getPicklistValues, {
+    //     recordTypeId: '$recordTypeId',
+    //     fieldApiName: ACCOUNT_SOURCE_FIELD
+    // })
+    // wiredPicklist ({ data, error }) {
+    //     if (data)
+    //     {
+    //         console.log("Picklist Data fetched .. \n", data.values);
+    //         this.options=data.values.map(currItem => {
+    //             return {
+    //                 'label': currItem.label,
+    //                 'value':currItem.value
+    //             }
+    //         })
+    //     }
+    //     else if (error)
+    //     {
+    //         console.log(error)
+    //     }
+    // }
+    
+    @wire(getPicklistValuesByRecordType, {
         recordTypeId: '$recordTypeId',
-        fieldApiName: ACCOUNT_SOURCE_FIELD
+        objectApiName: '$objectApiName'
     })
     wiredPicklist ({ data, error }) { 
         if (data)
         {
-            console.log("Picklist Data fetched .. \n", data.values);
-            this.options=data.values.map(currItem => { 
-                return {
-                    'label': currItem.label,
-                    'value':currItem.value
-                }
-            })
+            console.log("Picklist Data fetched .. \n", data);
+            if (data.picklistFieldValues && data.picklistFieldValues[this.fieldApiName])
+            { 
+                let picklistValues = data.picklistFieldValues[this.fieldApiName];
+                this.options = picklistValues.values;
+            }
         }
         else if (error)
         { 
